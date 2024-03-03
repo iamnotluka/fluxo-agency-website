ae12a8601af97603491f2315c40e92c8b0cfd9d0
iamnotluka <zoricluk@gmail.com>
2024-03-03 21:53:24 +1000

    Add the thing where each time we change the page it scrolls to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import "./App.css";
 import HyperScaleRoute from "./router/HyperScaleRoute";
 import LandingRoute from "./router/LandingRoute";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import AboutRoute from "./router/AboutRoute";
 import BlogsRoute from "./router/BlogsRoute";
 import TermsAndConditionsRoute from "./router/TermsAndConditionsRoute";
 import PrivacyPolicyRoute from "./router/PrivacyPolicyRoute";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
+}
 
 function App() {
 	return (
 		<div>
 			<BrowserRouter>
+				<ScrollToTop /> {/* Include ScrollToTop component inside BrowserRouter */}
 				<Routes>
 					<Route path="*" element={<LandingRoute />}></Route>
 					<Route path="hyper-scale-automation" element={<HyperScaleRoute />}></Route>
@@ -24,5 +36,4 @@ function App() {
 		</div>
 	);
 }
-
 export default App;
